3fcf283aba8aae52897fd916002b016f285c267c
Alejandro Vázques O <allvaso2@gmail.com>
2020-05-04 19:26:30 -0600

    Inclusión rapidjson y pruebas abrir, parsear, leer, crear, cambiar JSON

--- VSC_ext/main.cpp
+++ VSC_ext/main.cpp
@@ -4,6 +4,18 @@
 #include <thread>
 #include "pthread.h"
 #include <unistd.h>
+#include "include/rapidjson/document.h"
+#include "include/rapidjson/istreamwrapper.h"
+#include "include/rapidjson/writer.h"
+#include "include/rapidjson/stringbuffer.h"
+#include "include/rapidjson/ostreamwrapper.h"
+#include <fstream>
+
+
+using namespace rapidjson;
+using namespace std;;
+
+
 bool* flag = new bool(true);
 void algo(){
     while(*flag){
@@ -15,7 +27,7 @@ void algo(){
 };
 
 int main(){
-    GarbageCollector* rec = GarbageCollector::getGarbageCollector();
+    /*GarbageCollector* rec = GarbageCollector::getGarbageCollector();
     for(int i =0; i<100001; i++){
         rec->add_Pkg_To_List(new specific_package<int>(i,10+i));
     }
@@ -29,5 +41,32 @@ int main(){
     std::cin.get();
     *flag = false;
     prueba.join();
-    delete flag;
+    delete flag; */
+
+    ifstream ifs("JSONFiles/prueba.json");
+    IStreamWrapper isw (ifs);
+    Document root;
+    root.ParseStream(isw);
+
+    StringBuffer buffer;
+    Writer<StringBuffer> writer(buffer);
+    root.Accept(writer);
+
+    std::string jsonStr(buffer.GetString());
+        /*if(jsonStr == "null")
+            std::cout << "is null..." << std::endl; //<--always here!
+        else
+        {
+            std::cout << jsonStr.c_str() << std::endl;
+
+            root["ip"] = "123456789";
+
+            ofstream ofs("JSONFiles/prueba.json");
+            OStreamWrapper osw(ofs);
+            Writer<OStreamWrapper> writer2(osw);
+            root.Accept(writer2);
+        }*/
+    printf(root["ip"].GetString());
+
+
 }
