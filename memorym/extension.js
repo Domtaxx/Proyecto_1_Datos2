db0b65e232009734cf2be059c0d69e9048ff6a04
Alejandro Vázques O <allvaso2@gmail.com>
2020-05-28 23:52:46 -0600

    Interfaz obtención datos usuario para conexión remote memory

--- memorym/extension.js
+++ memorym/extension.js
@@ -2,8 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 const net = require('net');
-const ffi = require('ffi-napi');
-
+//const dom = new jsdom.JSDOM(serverDatos());
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
@@ -20,13 +19,16 @@ function activate(context) {
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('memorym.helloWorld', function () {
+		
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
 
+
 		var Client = net.createConnection;
 		var client = Client({port: 54000, localAddress: '127.0.0.1', localPort: 51000});
 		var datos = " ";
+		
 		
 
 		client.on('connect', function() {
@@ -35,7 +37,6 @@ function activate(context) {
 		client.write("$i")
 		});
 
-		
 		
 		var htmlText = `<!DOCTYPE html>
 		<html lang="en">
@@ -50,34 +51,53 @@ function activate(context) {
 			datos = ''+data;
 			console.log('Received: '+ data);
 
-			var pkgs = datos.split('&')[0].split(".");
-			var cantidadpkgs = pkgs.length;
-			
-			var ptrs = datos.split('&')[1].split(".");
-			var cantidadptrs = ptrs.length;
-			for(let i = 0; i<cantidadpkgs; i++){
-				var temp = pkgs[i].split(',')
-				htmlText+=`<p>Variable `+i.toString() + `</p> 
-				<p style="padding-left: 30px;">Tipo: `+temp[1]+
-				`<br />Valor: ` +temp[2]+
-				`<br />Celda de memoria: ` +temp[3]+
-				`<br />Referencias actuales: ` +temp[4]+`</p>`;
-			}
-			for(let i = 0; i<cantidadptrs; i++){
-				var temp = ptrs[i].split(',')
-				htmlText+=`<p>Puntero `+i.toString() + `</p> 
-				<p style="padding-left: 30px;">Id: `+temp[0]+
-				`<br />Tipo: ` +temp[1]+
-				`<br />Valor: ` +temp[2]+`</p>`;
-			}htmlText += `</body>
+			var valores = datos.split(',');
+			if(valores[0]== "0"){
+				htmlText+=`<p>Variable `+valores[1].toString() + `</p> 
+				<p style="padding-left: 30px;">Tipo: `+valores[2]+
+				`<br />Valor: ` +valores[3]+
+				`<br />Celda de memoria: ` +valores[4]+
+				`<br />Referencias actuales: ` +valores[5]+`</p>`;
+			}else if(valores[0] == "1"){
+				htmlText+=`<p>Puntero `+valores[1].toString() + `</p> 
+				<p style="padding-left: 30px;">Id: `+valores[1]+
+				`<br />Tipo: ` +valores[2]+
+				`<br />Valor: ` +valores[3]+`</p>`;
+			}else if(datos.endsWith(";")){
+
+				htmlText += `</body>
 							</html>`;
-			panel.webview.html = htmlText;
+				panel.webview.html = htmlText;
+			}
 		});
 
 		const panel = vscode.window.createWebviewPanel('memoryManager', 
 		'Memory Manager',vscode.ViewColumn.One,{});
 		
+		const serverData = vscode.window.createWebviewPanel('datosConexion', 
+		'Datos de conexion',vscode.ViewColumn.One,{
+			enableScripts:true
+		});
 		
+		serverData.webview.html = serverInfo;
+		serverData.webview.onDidReceiveMessage(
+			message => {
+				switch(message.command){
+					case 'data':
+						console.log(message.text);
+						//client.write(message.text); ENVIAR DATOS AL SERVIDOR 
+						return;
+					case 'leak':
+						vscode.window.showInformationMessage('No se recibieron los datos necesarios');
+					case 'stop':
+						console.log(message.text);
+						//client.write(message.text); DETENER CONEXIÓN CON REMOTE MEMORY
+				}
+			},
+			undefined,
+			context.subscriptions
+		);
+
 		vscode.window.showInformationMessage('Hello World from memoryM!');
 		
 	});
@@ -86,6 +106,58 @@ function activate(context) {
 }
 exports.activate = activate;
 
+//FALTA INCORPORAR FUNCIÓN RESET
+const serverInfo = `<!DOCTYPE html> 
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Datos de conexión</title>
+	<script type = "text/javascript">
+	function conectionData(){
+		const vscode = acquireVsCodeApi();
+		const user = document.getElementById('user').value;
+		const password = document.getElementById('password').value;
+		const ip = document.getElementById('ip').value;
+		const port = document.getElementById('port').value;
+		console.log(user);
+		if(user != "" && password != "" && ip != "" && port != ""){
+			vscode.postMessage({
+				command: 'data',
+				text: "true,"+user +","+ password +","+ ip +","+ port
+			})
+		}else{
+			vscode.postMessage({
+				command: 'leak',
+				text: ""
+			})
+		}
+	}
+	function killConection(){
+		const vscode = acquireVsCodeApi();
+		vscode.postMessage({
+			command: 'stop',
+			text: "false,0,0,0,0"
+		})
+	}
+
+	</script>
+</head>
+<body>
+	<form id="form" onSubmit="JavaScript:conectionData()">
+		<label for="user">Nombre de usuario:</label><br>
+		<input type="text" id="user" name="user" value=""><br>
+		<label for="password">Contraseña:</label><br>
+		<input type="password" id="password" name="password" value=""><br>
+		<label for="ip">IP:</label><br>
+		<input type="text" id="ip" name="ip" value=""><br>
+		<label for="port">Puerto:</label><br>
+		<input type="text" id="port" name="port" value=""><br><br>
+		<input type="submit" value="Aceptar">
+		<input type="reset">
+		</form> 
+</body>
+</html>`;
 // this method is called when your extension is deactivated
 function deactivate() {}
 
